bfc8e6bd12888f2de4b3f7ee78af7d4b4b6c41f8
KIAN <239125237+elysrismbusiness-jpg@users.noreply.github.com>
2026-01-17 21:23:16 +0000

    f

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/upgraded-carnival/',
+  base: '/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
